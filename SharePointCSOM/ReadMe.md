3fc415c2ce0f2b7ea114c0d666b063b157baab75
xluo <1196665422@qq.com>
2019-07-24 15:19:24 -0400

    SharePoint CSOM API

---
+++ SharePointCSOM/ReadMe.md
@@ -0,0 +1,3 @@
+用于测试SharePoint CSOM API
+
+以及做SharePoint 用
---
+++ SharePointCSOM/SharePointCSOMAPI/SharePointCSOMAPI/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointCSOMAPI
+{
+    class Program
+    {
+        private static ClientContext context = null;
+        static void Main(string[] args)
+        {
+        }
+
+        private static SecureString GetPassword(string password)
+        {
+            SecureString pwd = new SecureString();
+            foreach (char c in password)
+            {
+                pwd.AppendChar(c);
+            }
+            return pwd;
+        }
+
+        private static ClientContext Context
+        {
+            get
+            {
+                if (context == null)
+                {
+                    string siteUrl = "https://longgod.sharepoint.com/sites/XluoTest1";
+                    string userName = "aosiptest@longgod.onmicrosoft.com";
+                    string password = "demo12!@";
+                    context = new ClientContext(siteUrl);
+                    context.Credentials = new SharePointOnlineCredentials(userName, GetPassword(password));
+                    context.ExecuteQuery();
+                }
+
+                //if (context == null)
+                //{
+                //    string siteUrl = "https://tingtest123.sharepoint.com/sites/Shen01";
+                //    string userName = "TingTing@TingTest123.onmicrosoft.com";
+                //    string password = "1qaz2wsx!@";
+                //    context = new ClientContext(siteUrl);
+                //    context.Credentials = new SharePointOnlineCredentials(userName, GetPassword(password));
+                //    context.ExecuteQuery();
+                //}
+                return context;
+            }
+        }
+    }
+}
