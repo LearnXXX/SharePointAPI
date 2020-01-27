aaa0698f59b6b181c26afcf0e55e3c2099626494
xluo <1196665422@qq.com>
2020-01-27 20:33:16 +0800

    Support Graph object

--- SharePointGraph/SharePointGraph/Program.cs
+++ SharePointGraph/SharePointGraph/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Identity.Client;
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Newtonsoft.Json.Linq;
 using System;
@@ -7,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
+using System.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,21 @@ namespace SharePointGraph
         private const string TenantId = "65001581-c366-4764-80ab-aef9bc86ecca";
         private const string ClientSecret = "64f=:]DsKCoZP9kfXJlw1EpTRwDn?N6M";
 
+
+        static X509Certificate2 LoadCertificate()
+        {
+            //var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SharePointGraph.exportedcert.pfx");
+
+            using (var stream = System.IO.File.OpenRead(@"C:\Users\xluo\Desktop\XluoCert.pfx"))
+            {
+                using (var binaryReader = new BinaryReader(stream))
+                {
+                    var rawData = binaryReader.ReadBytes((Int32)stream.Length);
+                    return new X509Certificate2(rawData, "demo12!@");
+                }
+            }
+        }
+
         private static byte[] GetCertificateBytes()
         {
             using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SharePointGraph.ReferenceFiles.RestAPICertificate.pfx"))
@@ -27,6 +44,101 @@ namespace SharePointGraph
                 stream.CopyTo(memoryStream);
                 return memoryStream.ToArray();
             }
+        }
+        private static List<T> GetRequestAllOfDatas<T>(dynamic currenPage, List<T> datas)
+        {
+            datas.AddRange(currenPage.CurrentPage as List<T>);
+            if (currenPage.NextPageRequest != null)
+            {
+                var nextPage = currenPage.NextPageRequest.GetAsync().Result;
+                GetRequestAllOfDatas(nextPage, datas);
+            }
+            return datas;
+        }
+
+        private static List<DriveItem> GetAllSubFolders(GraphServiceClient graphServiceClient, string driveId, string folderId)
+        {
+            List<DriveItem> allOfFolders = new List<DriveItem>();
+            List<DriveItem> subItmes = new List<DriveItem>();
+            var currentPage = graphServiceClient.Drives[driveId].Items[folderId].Children.Request().GetAsync().Result;
+            GetRequestAllOfDatas(currentPage, subItmes);
+            var subFolders = subItmes.Where(item => item.Folder != null).ToList();
+
+            foreach (var child in subFolders)
+            {
+                if (child.Folder.ChildCount > 0)
+                {
+                    allOfFolders.AddRange(GetAllSubFolders(graphServiceClient, driveId, child.Id));
+                }
+            }
+            allOfFolders.AddRange(subFolders);
+            return allOfFolders;
+        }
+
+        private static void GraphTest()
+        {
+            var clientId = "50927317-52bf-40c0-a4f1-9f19d04049a0";
+            var tenantId = "b4b8748a-a573-4e49-8665-3a632b65a60c";
+            var application = ConfidentialClientApplicationBuilder.Create(clientId)
+                        .WithCertificate(LoadCertificate())
+                        .WithAuthority(AzureCloudInstance.AzurePublic, tenantId, false)
+                        .Build();
+            var token = application.AcquireTokenForClient(new List<string> { "https://graph.microsoft.com/.default" }).ExecuteAsync().Result.AccessToken;
+
+            var graphService = new GraphServiceClient("https://graph.microsoft.com/v1.0", new DelegateAuthenticationProvider(a =>
+            {
+                a.Headers.Add("Authorization", "Bearer " + token);
+                return Task.FromResult(0);
+            }));
+            var siteUrl = "https://m365x157144-my.sharepoint.com/personal/admin_m365x157144_onmicrosoft_com";
+            var siteUri = new Uri(siteUrl);
+
+            var fields = graphService.Sites["m365x157144-my.sharepoint.com,47b37a14-09dd-407f-9509-6fa9b4ad20d4,08d6db6f-5165-4fd4-ad22-7a61201f766a"].Lists["a44cb590-eb05-45d4-bf28-30f73385cd3e"].Request().Expand("columns").GetAsync().Result;
+            var itemsss = graphService.Sites["m365x157144-my.sharepoint.com,47b37a14-09dd-407f-9509-6fa9b4ad20d4,08d6db6f-5165-4fd4-ad22-7a61201f766a"].Lists["a44cb590-eb05-45d4-bf28-30f73385cd3e"].Drive.Root.ItemWithPath("ffb").Children.Request().GetAsync().Result;
+            var allFolders = GetAllSubFolders(graphService, "b!KuhVBiISlE2UiUGxsisMZwa-BXE1X3dBpZTQWlyss7B_-Pp1Ze-FTrovkzVI5c-j", "01AIHEEQF6Y2GOVW7725BZO354PWSELRRZ");
+
+
+
+
+            var driveList = graphService.Sites["m365x157144-my.sharepoint.com,47b37a14-09dd-407f-9509-6fa9b4ad20d4,08d6db6f-5165-4fd4-ad22-7a61201f766a"].Lists["a44cb590-eb05-45d4-bf28-30f73385cd3e"].Drive.Root.Request().GetAsync().Result;
+            var items = graphService.Drives["b!FHqzR90Jf0CVCW-ptK0g1G_b1ghlUdRPrSJ6YSAfdmqQtUykBevURb8oMPczhc0-"].Items.Request().GetAsync().Result;
+            var list = graphService.Sites["m365x157144-my.sharepoint.com,47b37a14-09dd-407f-9509-6fa9b4ad20d4,08d6db6f-5165-4fd4-ad22-7a61201f766a"].Lists.Request().GetAsync().Result;
+
+            var drive = graphService.Drives["b!FHqzR90Jf0CVCW-ptK0g1G_b1ghlUdRPrSJ6YSAfdmqQtUykBevURb8oMPczhc0-"].Items["01Z2O2D6OOCLSJHS435ZDJEGBMCLVP7YG4"].Versions["1.0"].Content.Request().GetAsync().Result;
+
+            var memberStream = new MemoryStream();
+            drive.CopyTo(memberStream);
+            System.IO.File.WriteAllBytes(@"C:\Users\xluo\Desktop\File2.docx", memberStream.ToArray());
+            var driveItem = graphService.Drive.Items["01Z2O2D6OOCLSJHS435ZDJEGBMCLVP7YG4"].Request().GetAsync().Result;
+
+            var result = graphService.Sites.GetByPath("/personal/admin_m365x157144_onmicrosoft_com", "m365x157144-my.sharepoint.com").Request().GetAsync().Result;
+
+
+        }
+        private static void ADALUsernameTest()
+        {
+            var clientId = "12128f48-ec9e-42f0-b203-ea49fb6af367";
+            var tenantId = "b4b8748a-a573-4e49-8665-3a632b65a60c";
+            var application = PublicClientApplicationBuilder.Create(clientId)
+             .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
+             //.WithRedirectUri("urn:ietf:wg:oauth:2.0:oob")
+             .Build();
+
+            var username = "admin@M365x157144.onmicrosoft.com";
+            var password = "X60LyQ995R";
+            var url = "https://graph.microsoft.com/.default";
+            var authenticationResult = application.AcquireTokenByUsernamePassword(new List<string> { url.ToString() }, username, GetPassword(password))
+                    .ExecuteAsync().Result;
+
+        }
+        private static SecureString GetPassword(string password)
+        {
+            SecureString pwd = new SecureString();
+            foreach (char c in password)
+            {
+                pwd.AppendChar(c);
+            }
+            return pwd;
         }
         static void Main(string[] args)
         {
